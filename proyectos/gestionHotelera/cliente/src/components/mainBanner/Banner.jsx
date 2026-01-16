e093625a652bfe6b17dda7f87bb6463dd9b5dbcf
Julián Castro Barrantes <167035555+Jcastro1605@users.noreply.github.com>
2026-01-15 19:29:45 -0600

    Banner terminado

--- proyectos/gestionHotelera/cliente/src/components/mainBanner/Banner.jsx
+++ proyectos/gestionHotelera/cliente/src/components/mainBanner/Banner.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./banner.module.css"
 import { Link } from "react-router-dom";
+import animationStyles from "./../animations/loadIn.module.css";
 
 function Banner() {
 
@@ -8,9 +9,9 @@ function Banner() {
     <div className={styles.banner}>
         <div className={styles.shadow}>
             <div className={styles.textContainer}>
-                <h1 className={styles.title}><span>Tortuga Verde</span><br/>
+                <h1 className={`${styles.title} ${animationStyles.fadeUp}` }><span>Tortuga Verde</span><br/>
                 Descubre el paraíso en Limón</h1> 
-                <h2>Encuentra hoteles, habitaciones y actividades todo en un único lugar. </h2>    
+                <h2 className={`${animationStyles.fadeUp} ${animationStyles.delay1}`}>Encuentra hoteles, habitaciones y actividades todo en un único lugar. </h2>    
                 <div className={styles.btnContainer}>
                     <Link to="/" className={styles.linkBtn}>Ver Habitaciones</Link>
                     <Link to="/" className={styles.linkBtn}>Ver Actividades Recreativas</Link>

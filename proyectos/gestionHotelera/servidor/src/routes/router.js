a91e5cf0aa87fc8714b310e3f1a6d9f8d8e13a8e
Julián Castro Barrantes <167035555+Jcastro1605@users.noreply.github.com>
2026-01-24 23:01:15 -0600

    Login listo

--- proyectos/gestionHotelera/servidor/src/routes/router.js
+++ proyectos/gestionHotelera/servidor/src/routes/router.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import healthRouter from './health.js';
-import locationRoutes from './locationRoutes.js';  
-import registerRoute from './registerRoutes.js';
-
+import locationRoutes from './locationRoutes.js';
+import registerRoutes from './registerRoutes.js';
+import loginRoutes from './loginRoute.js';
 
 const router = express.Router();
 
 router.use('/health', healthRouter);
-router.use('/locations', locationRoutes);  
-router.use('/users', registerRoute);
+router.use('/locations', locationRoutes);
+router.use('/users', registerRoutes);
+router.use('/auth', loginRoutes);
 
 export default router;
